d4168538901c195aa2cc44372bf5db365dbe4c72
Jasdeep Jawanda <Jasdeep.Jawanda@ff.com>
2018-06-12 14:18:06 -0700

    course class

--- src/Course.java
+++ src/Course.java
@@ -1,4 +1,26 @@
-
 public class Course {
-
+    private String courseID;
+    private String courseName;
+    private int classSize;
+    
+    
+    Course(String id, String name, int size){
+        courseID = id;
+        courseName = name;
+        classSize  = size;
+    }
+    
+    public String getCourseName() {
+        return courseName;
+    }
+    
+    
+    public String getCourseID() {
+        return courseID;
+    }
+    
+    public int getClassSize() {
+        return classSize;
+    }
+   
 }
