c3c2545e1b4286b519ae62ed0e02e41ac2074a95
hkneal <hiramk@comcast.net>
2018-06-12 17:18:36 -0700

    Updating Registration class & File.txt

--- src/Registration.java
+++ src/Registration.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-public class Registration {
+public class RegistrationB {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -11,7 +11,7 @@ public class Registration {
 		System.out.println("Welcome to the Student Registration Page!");
 		System.out.println();
 		System.out.println("Listed below are all the courses that are available for registration this semister.");
-		System.out.println("Please enter in the course number only to register for a course, eg. \"101\" for the \"101 - Basics of Java\" course.");
+		System.out.println();
 		
 		//Read course information for display from txt file
 		File file = new File("Files/availableCourses.txt");
@@ -21,7 +21,7 @@ public class Registration {
 
 			while (sc.hasNextLine()) {
 				String line = sc.nextLine();
-			    String[] elements = line.split(",");
+			    String[] elements = line.split(", ");
 			    if(elements.length == 3) {
 			    		availableCourses.add(new Course(elements[0], elements[1], Integer.parseInt(elements[2])));
 			    }
@@ -37,10 +37,14 @@ public class Registration {
 		for(Course course : availableCourses) {
 			System.out.println(course.getCourseID() + ", " + course.getCourseName());
 		}
+		System.out.println();
+		System.out.println("Please enter in the course number only to register for a course, eg. \"101\" for the \"101 - Java Basics\" course.");
 		
 //		Scanner sc = new Scanner(System.in);
 //		
 //		String[] inputValues = sc.nextLine().split(",");
+		//Test Input for valid number use switch case to search for proper course  
+		//Sort Arraylist and use bubble sort to find right course.
 
 	}
 	
